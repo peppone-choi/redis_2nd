1c2a92da7e1c8558a5dfc830f631707753d40f0a
peppone-choi <peppone.choi@gmail.com>
2025-03-16 15:49:52 +0900

    feat: 컨트롤러 제작

---
+++ module-api/src/main/java/com/pepponechoi/cinema/movie/controller/MovieController.java
@@ -0,0 +1,23 @@
+package com.pepponechoi.cinema.movie.controller;
+
+import com.pepponechoi.cinema.movie.dto.response.MovieResponse;
+import com.pepponechoi.cinema.movie.service.MovieService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/movies")
+@RequiredArgsConstructor
+public class MovieController {
+    private final MovieService service;
+
+    @GetMapping
+    public ResponseEntity<List<MovieResponse>> findAll() {
+        List<MovieResponse> allMoviesIsShowing = service.findAllMoviesIsShowing();
+        return ResponseEntity.ok(allMoviesIsShowing);
+    }
+}
