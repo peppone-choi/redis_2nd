acc6d61a2f78154fb269bd1aaa63522bb939e07c
peppone-choi <peppone.choi@gmail.com>
2025-03-16 12:51:40 +0900

    feat: ScreenRepository 메소드 작성

--- module-domain/src/main/java/com/pepponechoi/cinema/screen/repository/ScreenRepository.java
+++ module-domain/src/main/java/com/pepponechoi/cinema/screen/repository/ScreenRepository.java
@@ -1,5 +1,14 @@
 package com.pepponechoi.cinema.screen.repository;
 
-public interface ScreenRepository {
+import com.pepponechoi.cinema.screen.entity.Screen;
+import java.util.Collection;
+import java.util.List;
 
+public interface ScreenRepository {
+    Screen save(Screen screen);
+
+    Screen findById(int id);
+    List<Screen> findAll();
+
+    void deleteByIdsIn(Collection<Long> ids);
 }
