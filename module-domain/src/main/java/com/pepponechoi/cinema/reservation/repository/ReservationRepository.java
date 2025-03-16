57a6ad6a1abf174141384705c632834ef9e8b1d0
peppone-choi <peppone.choi@gmail.com>
2025-03-16 12:49:45 +0900

    feat: ReservationRepository 메소드 작성

--- module-domain/src/main/java/com/pepponechoi/cinema/reservation/repository/ReservationRepository.java
+++ module-domain/src/main/java/com/pepponechoi/cinema/reservation/repository/ReservationRepository.java
@@ -1,5 +1,15 @@
 package com.pepponechoi.cinema.reservation.repository;
 
-public interface ReservationRepository {
+import com.pepponechoi.cinema.reservation.entity.Reservation;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
 
+public interface ReservationRepository {
+    Reservation save(Reservation reservation);
+
+    Optional<Reservation> findById(Long id);
+    List<Reservation> findAll();
+
+    void deleteByIdsIn(Collection<Long> ids);
 }
