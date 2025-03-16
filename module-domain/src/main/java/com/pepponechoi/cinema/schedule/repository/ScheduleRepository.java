c8f9f8d34f56c636cf53de3340e847b2e16cc651
peppone-choi <peppone.choi@gmail.com>
2025-03-16 12:50:42 +0900

    feat: ScheduleRepository 메소드 작성

--- module-domain/src/main/java/com/pepponechoi/cinema/schedule/repository/ScheduleRepository.java
+++ module-domain/src/main/java/com/pepponechoi/cinema/schedule/repository/ScheduleRepository.java
@@ -1,5 +1,15 @@
 package com.pepponechoi.cinema.schedule.repository;
 
-public interface ScheduleRepository {
+import com.pepponechoi.cinema.schedule.entity.Schedule;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
 
+public interface ScheduleRepository {
+    Schedule save(Schedule schedule);
+
+    Optional<Schedule> findById(Long id);
+    List<Schedule> findAll();
+
+    void deleteByIdsIn(Collection<Long> ids);
 }
